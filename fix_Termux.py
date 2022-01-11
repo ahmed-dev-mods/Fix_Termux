f87770b2bb6f56bf7816e5df8076f36165ef6241
Ahmed-dev-mods <ahmed.dev.mods@gmail.com>
2022-01-12 01:25:31 +0300

    initial commit

---
+++ fix_Termux.py
@@ -0,0 +1,227 @@
+##################################################################
+# library
+import os
+import sys
+import webbrowser
+
+#################################################################
+black = '\033[0;30m'
+red = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+red1 = '\033[2;31m' #احمر ثاني
+F = '\033[2;32m' #اخضر
+A = '\033[2;34m'#ازرق
+C = '\033[2;35m' #وردي
+C = "\033[1;97m" #ابيض
+B = '\033[2;36m'#سمائي
+Y = '\033[1;34m' #ازرق فاتح
+E = '\033[1;31m' #احمر
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+clear() 
+
+#####################################banner#####################################################
+def banner():
+    print("                                                                 ")
+    print(red1+"  ███████╗██╗██╗  ██╗    ████████╗███████╗██████╗ ███╗   ███╗██╗   ██╗██╗  ██╗")
+    print(red1+"  ██╔════╝██║╚██╗██╔╝    ╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║   ██║╚██╗██╔╝")
+    print(C+"  █████╗  ██║ ╚███╔╝        ██║   █████╗  ██████╔╝██╔████╔██║██║   ██║ ╚███╔╝")
+    print(C+"  ██╔══╝  ██║ ██╔██╗        ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║   ██║ ██╔██╗")
+    print(black+"  ██║     ██║██╔╝ ██╗       ██║   ███████╗██║  ██║██║ ╚═╝ ██║╚██████╔╝██╔╝ ██╗")
+    print(black+"  ╚═╝     ╚═╝╚═╝  ╚═╝       ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝"+C+"Yemen")
+    print(B+"                 Tool"+F+" By"+C+"  AHMED-DEV-MODS")
+
+banner()
+###################################plugins#############################################
+def plugins():
+    print("=>"*60)
+    os.system("apt-get update -y")
+    os.system("apt-get upgrade -y")
+    os.system("apt-get install python -y")
+    os.system("apt-get install python2 -y")
+    os.system("apt-get install python3 -y")
+    os.system("apt-get install ruby -y")
+    os.system("apt-get install git -y")
+    os.system("apt-get install php -y")
+    os.system("apt-get install nano -y")
+    os.system("apt-get install nmap -y")
+    os.system("apt-get install perl -y")
+    os.system("termux-setup-storage -y")
+    os.system("apt-get install golang -y")
+    os.system("apt-get install host -y")
+    os.system("apt-get install havij -y")
+    os.system("apt-get install hydra -y")
+    os.system("apt install wireshark -y")
+    os.system("apt-get install cmatrix -y")
+    os.system("apr-get install figlet -y")
+    os.system("apt-get install wget -y")
+    os.system("apt-get install wget -y")
+    os.system("apt-get install python2 -y")
+    os.system("apt-geg install python2-dev -y")
+    os.system("apt install wireshark -y")
+    os.system("apt-get install cowsay -y")
+    os.system("apt-get install toilet -y")
+    os.system("apt-get install curlinstall wgetrc -y")
+    os.system("apt-get install ruby -y")
+    os.system("apt-get install help -y")
+    os.system("apt-get install net-tools -y")
+    os.system("apt-get install w3m -y")
+    os.system("apt-get install unrar -y ")
+    os.system("apt-get install clang -y")
+    os.system("apt-get install openssh -y")
+    os.system("apt-get install tor -y")
+    os.system("apt-get install tar -y")
+    os.system("apt-get install zip -y")
+    os.system("apt-get install proot -y")
+    os.system("pip2 install wget -y")
+    os.system("pip2 install requests -y")
+    os.system("gem install lolcat -y")
+    os.system("apt update -y && apt upgrade -y")
+    os.system('cls' if os.name == 'nt' else 'clear') 
+    print(red1+"≠"*60)
+    print(B+'Successfully downloaded ')
+    
+###################################Libray#############################################
+def Libray():
+    print("∞"*60)
+    os.system('pip install requests')
+    os.system('pip install pyfiglet')
+    os.system('pip install sys')
+    os.system('pip install py_compile')
+    os.system('pip install youtube_dl')
+    os.system('pip install uuid')
+    os.system('pip install time')
+    os.system('pip install os')
+    os.system('pip install random')
+    os.system('pip install datetime')
+    os.system('pip install string')
+    os.system('pip install secrets')
+    os.system('pip install webbrowser')
+    os.system('pip install hashlib')
+    os.system('pip install colorama')
+    os.system('pip install n')
+    os.system('pip install mm')
+    os.system('pip install new')
+    os.system('pip install sleep')
+    os.system('pip install BeautifulSoup')
+    os.system('pip install new')
+    os.system('pip install pafy')
+    os.system('pip install bs4')
+    os.system('pip install sys')
+    os.system('pip install json')
+    os.system('pip install random')
+    os.system('pip install uuid')
+    os.system('pip install secrets')
+    os.system('pip install datetime')
+    os.system('pip install *')
+    os.system('pip install argparse')
+    os.system('pip install InstagramAPI')
+    os.system('pip install sleep')
+    os.system('pip install string')
+    os.system('pip install uuid4')
+    os.system('pip install generate_user_agent')
+    os.system('pip install threading')
+    os.system('pip install json')
+    os.system('pip install datetime')
+    os.system('pip install token_hex')
+    os.system('pip install secrets')
+    os.system('pip install token_hex')
+    os.system('pip install Fore')
+    os.system('pip install secrets')
+    os.system('pip install uuid')
+    os.system('pip install re')
+    os.system('pip install b')
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(red1+"≠"*60)
+    print(B+'Successfully downloaded ')
+    
+####################################menu###########################################
+
+loop = True
+def menu():
+    print(B+"●"+red1+"═══════════════════════════════════════════════════════"+B+"●")
+    print(red1+"["+F+"1"+red1+"]"+B+"➥"+C+ " Install Plugins")
+    print(red1+"["+F+"2"+red1+"]"+B+"➥"+C+ " Install Libray")
+    print(red1+"["+F+"3"+red1+"]"+B+"➥"+C+ " Install Termux Style Free")
+    print(red1+"["+F+"4"+red1+"]"+B+"➥"+C+ " Install keyboard Hacker")
+    print(red1+"["+F+"5"+red1+"]"+B+"➥"+C+ " Telegram")
+    print(red1+"["+F+"6"+red1+"]"+B+"➥"+C+ " Youtube")
+    print(red1+"["+F+"00"+red1+"]"+B+"➥"+red1+ " Exit")
+    print(B+"●"+red1+"═══════════════════════════════════════════════════════"+B+"●")
+
+while loop:
+    menu()
+    what = input(B+"FIX_Termux"+red1+"$>"+F+"")
+    if what == "1":
+        print(C+"•-------------------------•")
+        install_plugins = input(B+"["+red1+"?"+B+"]"+C+" Are you sure to download plugins?"+X+"("+F+"y"+B+"/"+red1+"n"+X+")"+C+":")
+        print(C+"•-------------------------•")
+        if install_plugins == "y":
+            print("Wait a while.....")
+            plugins()
+            ########تحقق الشرط
+            rmenu = input(B+"["+red1+"?"+B+"]"+C+" Back to Menu? "+X+"("+F+"y"+B+"/"+red1+"n"+X+")"+C+":")
+            if rmenu == "y":
+                clear()
+                banner()
+                menu()
+            else:
+                break
+    elif what == "2":
+        print(C+"•-------------------------•")
+        install_libraries = input(B+"["+red1+"?"+B+"]"+C+" Are you sure to download libraries? "+X+"("+F+"y"+B+"/"+red1+"n"+X+")"+C+":")
+        print(C+"•-------------------------•")
+        if install_libraries == "y":
+            print("Wait a while.....")
+            Libray()
+            ########تحقق الشرط
+            rmenu = input(B+"["+red1+"?"+B+"]"+C+" Back to Menu? "+X+"("+F+"y"+B+"/"+red1+"n"+X+")"+C+":")
+            if rmenu == "y":
+                menu()
+            else:
+                break
+    elif what == "3":
+        webbrowser.open('https://hole.apkdone.download/gp.php?token=AOTCm0S3BkTeaOz2p8PIzAMFUS6ZomZDI7Q6fWRhfhOF5nYzZu0b1HAB0mHU0DPh1aP9Tg1GQJC9CXmRGuTCdqMVbpm3FbPQ6QEq_Y4bo511biKtlwCqyQgrGLwtp6phh3Uyp1hUmuuMd9PIIbfmIhpESNiHFfko9vP9tLT60436R7XOAMtHS1OuXx8sStw2UL6k0A-as22PFlhy2-457LMAE2iE1wNNmFbeCxsLExK60MfG_Lg8ka6nC5U8KujD499GnlxjkejkrAOQzyaFe8QLz4gIKfBQLiW8JcGbK-oXnl6EDH8Jx8ygkVPE-Fni2c339TnludJQbOAutW-l6cmU3-EGaF3ijU_1HxJvkomlepL-bAI36W6GZ2slTpFvnM319aLMRca8eev8REc0dKoA2g&cpn=RlhmcLIiuY7GT-Lj&server=boq&name=com.termux.styling_apkdone.com.apk&pkg=com.termux.styling')
+        print("Termux Style free")
+        rmenu = input(B+"["+red1+"?"+B+"]"+C+" Back to Menu? "+X+"("+F+"y"+B+"/"+red1+"n"+X+")"+C+":")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "4":
+        webbrowser.open('https://play.google.com/store/apps/details?id=org.pocketworkstation.pckeyboard')
+        print("keyboard Hacker")
+        rmenu = input(B+"["+red1+"?"+B+"]"+C+" Back to Menu? "+X+"("+F+"y"+B+"/"+red1+"n"+X+")"+C+":")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "5":
+        webbrowser.open("https://t.me/ahmed_dev_mods")
+        rmenu = input(B+"["+red1+"?"+B+"]"+C+" Back to Menu? "+X+"("+F+"y"+B+"/"+red1+"n"+X+")"+C+":")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "6":
+        webbrowser.open("https://www.youtube.com/channel/UCmFOcRw0kbeazJK3tLy_cMw")
+        rmenu = input(B+"["+red1+"?"+B+"]"+C+" Back to Menu? "+X+"("+F+"y"+B+"/"+red1+"n"+X+")"+C+":")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "00":
+        print("Good Bye 🤝")
+        break
+
+#########################script################################
+# code open youtub
+#webbrowser.open("https://www.youtube.com/channel/UCmFOcRw0kbeazJK3tLy_cMw")
+# code clare terminal
+
+
+
+
+#Are you sure to download libraries?
+#Back to Menu?
